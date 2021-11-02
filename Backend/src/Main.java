be56a5163505f15a29096fadac0e1e6db284ce02
Dubsky <dubcmsky@protonmail.com>
2021-11-02 21:46:01 +0100

    update Main

--- Backend/src/Main.java
+++ Backend/src/Main.java
@@ -13,9 +13,10 @@ public class Main {
   private static Server server;
   private static Database database;
 
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) throws SQLException, IOException {
     //server = new Server(25565);
     database = new Database();
+    database.viewUsers();
   }
 
 }
