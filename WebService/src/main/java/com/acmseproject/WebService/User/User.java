62451059434e71c7721de04d8b32ab425d0ac7d9
Dubsky <dubcmsky@protonmail.com>
2021-11-04 04:13:44 +0100

    add User class

---
+++ WebService/src/main/java/com/acmseproject/WebService/User/User.java
@@ -0,0 +1,32 @@
+package com.acmseproject.WebService.User;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+
+    public User(int id, String name, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
