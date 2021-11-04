a79fa8a23a4b824cb111ed3cd67a0646527ce687
Dubsky <dubcmsky@protonmail.com>
2021-11-04 05:04:25 +0100

    add Data repository

---
+++ WebService/src/main/java/com/acmseproject/WebService/User/UserRepository.java
@@ -0,0 +1,11 @@
+package com.acmseproject.WebService.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findAll();
+}
