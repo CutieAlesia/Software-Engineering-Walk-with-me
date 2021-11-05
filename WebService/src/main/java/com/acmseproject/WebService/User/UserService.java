00b3ed0270f0fb55bea15223122526de4b525b68
Dubsky <dubcmsky@protonmail.com>
2021-11-05 22:38:56 +0100

    update UserService
    not used for now!

--- WebService/src/main/java/com/acmseproject/WebService/User/UserService.java
+++ WebService/src/main/java/com/acmseproject/WebService/User/UserService.java
@@ -1,7 +1,6 @@
 package com.acmseproject.WebService.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
