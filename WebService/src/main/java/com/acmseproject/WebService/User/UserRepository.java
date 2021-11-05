44b81ea81e0797cb2fde2be2cbcfbf5b813d9bb0
Dubsky <dubcmsky@protonmail.com>
2021-11-05 22:38:33 +0100

    update UserRepository
    added new callbacks

--- WebService/src/main/java/com/acmseproject/WebService/User/UserRepository.java
+++ WebService/src/main/java/com/acmseproject/WebService/User/UserRepository.java
@@ -1,11 +1,15 @@
 package com.acmseproject.WebService.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findAll();
+    User findByUsername(String username);
+    User findById(int id);
+    User findByEmail(String email);
+
+    @Query(value="SELECT * FROM se.user WHERE username = :user AND password = :pw",nativeQuery=true)
+    User login(String user, String pw);
 }
