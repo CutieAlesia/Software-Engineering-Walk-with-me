d512c6769f47078095db7f1c9842a60879ccc261
Dubsky <dubcmsky@protonmail.com>
2021-11-30 10:25:30 +0100

    refactored UserInfoRepository

--- WebService/src/main/java/com/acmseproject/WebService/UserInfo/UserInfoRepository.java
+++ WebService/src/main/java/com/acmseproject/WebService/UserInfo/UserInfoRepository.java
@@ -5,16 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import javax.transaction.Transactional;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * @author Dubsky
+ * @version 1.0
+ */
 @Repository
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer>{
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
 
     UserInfo findByUserid(int userid);
 
-    @Query(value="SELECT apikey FROM walkwithme.api_keys WHERE apikey = :key", nativeQuery=true)
+    @Query(value = "SELECT apikey FROM walkwithme.api_keys WHERE apikey = :key", nativeQuery = true)
     String checkAuth(String key);
 }
