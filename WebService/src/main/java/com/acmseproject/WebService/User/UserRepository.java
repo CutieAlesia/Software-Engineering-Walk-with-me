b1c26ffc9a68cdbc37c0bd9d3a1bcba2ce7d59d3
Dubsky <dubcmsky@protonmail.com>
2021-11-30 10:25:20 +0100

    refactored UserRepository

--- WebService/src/main/java/com/acmseproject/WebService/User/UserRepository.java
+++ WebService/src/main/java/com/acmseproject/WebService/User/UserRepository.java
@@ -4,27 +4,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import javax.transaction.Transactional;
 
+/**
+ * @author Dubsky
+ * @version 1.2
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
+
     User findById(int id);
+
     User findByEmail(String email);
 
-    @Query(value="SELECT * FROM walkwithme.user WHERE username = :username AND password = :password", nativeQuery=true)
+    @Query(value = "SELECT * FROM walkwithme.user WHERE username = :username AND password = :password", nativeQuery = true)
     User login(String username, String password);
 
     @Modifying(clearAutomatically = true)
-    @Query(value="UPDATE walkwithme.user SET email = :email WHERE id = :id", nativeQuery=true)
+    @Query(value = "UPDATE walkwithme.user SET email = :email WHERE id = :id", nativeQuery = true)
     @Transactional
     void changeEmail(int id, String email);
 
     @Modifying(clearAutomatically = true)
-    @Query(value="UPDATE walkwithme.user SET password = :password WHERE id = :id", nativeQuery=true)
+    @Query(value = "UPDATE walkwithme.user SET password = :password WHERE id = :id", nativeQuery = true)
     @Transactional
     void changePassword(int id, String password);
 
-    @Query(value="SELECT apikey FROM walkwithme.api_keys WHERE apikey = :key", nativeQuery=true)
+    @Query(value = "SELECT apikey FROM walkwithme.api_keys WHERE apikey = :key", nativeQuery = true)
     String checkAuth(String key);
 }
