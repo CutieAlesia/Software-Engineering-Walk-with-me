8cd0bd8581c6ae9006bc31a71290348f02549b58
Dubsky <dubcmsky@protonmail.com>
2021-12-14 23:31:33 +0100

    added post calls for attributes

--- WebService/src/main/java/com/acmseproject/WebService/UserInfo/UserInfoRepository.java
+++ WebService/src/main/java/com/acmseproject/WebService/UserInfo/UserInfoRepository.java
@@ -35,12 +35,45 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
 
     @Modifying(clearAutomatically = true)
     @Query(
-            value =
-                    "UPDATE walkwithme.user_info SET walkwithme.user_info.ranking = :newRank WHERE"
-                            + " userid = :id",
+            value = "UPDATE walkwithme.user_info SET bio = :bio WHERE userid = :id",
             nativeQuery = true)
     @Transactional
-    void changeRank(int id, int newRank);
+    void changeBio(int id, String bio);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = "UPDATE walkwithme.user_info SET gender = :gender WHERE userid = :id",
+            nativeQuery = true)
+    @Transactional
+    void changeGender(int id, String gender);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = "UPDATE walkwithme.user_info SET race = :race WHERE userid = :id",
+            nativeQuery = true)
+    @Transactional
+    void changeRace(int id, String race);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = "UPDATE walkwithme.user_info SET friendly = :friendly WHERE userid = :id",
+            nativeQuery = true)
+    @Transactional
+    void changeFriendly(int id, int friendly);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = "UPDATE walkwithme.user_info SET height = :height WHERE userid = :id",
+            nativeQuery = true)
+    @Transactional
+    void changeHeight(int id, int height);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = "UPDATE walkwithme.user_info SET weight = :weight WHERE userid = :id",
+            nativeQuery = true)
+    @Transactional
+    void changeWeight(int id, int weight);
 
     @Modifying(clearAutomatically = true)
     @Query(
@@ -48,6 +81,15 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
             nativeQuery = true)
     @Transactional
     void changeAvatar(int id, String image);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value =
+                    "UPDATE walkwithme.user_info SET walkwithme.user_info.ranking = :newRank WHERE"
+                            + " userid = :id",
+            nativeQuery = true)
+    @Transactional
+    void changeRank(int id, int newRank);
 
     @Query(value = "SELECT apikey FROM walkwithme.api_keys WHERE apikey = :key", nativeQuery = true)
     String checkAuth(String key);
