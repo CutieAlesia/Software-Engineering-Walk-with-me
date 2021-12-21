1f8331052c389e01898db98108258ec1ccd44b01
Kushurando <mnietzlo@gmail.com>
2021-12-21 17:04:05 +0100

    minor error handling fix

--- MobileApp/app/src/main/java/com/walkwithme/SecondFragment.java
+++ MobileApp/app/src/main/java/com/walkwithme/SecondFragment.java
@@ -144,9 +144,8 @@ public class SecondFragment extends Fragment {
 
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                VolleyLog.d(
-                                        TAG, "No more users to display, please try again later.");
-                                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT)
+                                VolleyLog.d(TAG, "No more users to display, please try again later.");
+                                Toast.makeText(getContext(), "No more users to display, please try again later.", Toast.LENGTH_SHORT)
                                         .show();
                                 NavHostFragment.findNavController(SecondFragment.this)
                                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
@@ -210,7 +209,7 @@ public class SecondFragment extends Fragment {
                             @Override
                             public void onErrorResponse(VolleyError error) {
                                 VolleyLog.d(TAG, "No more users to display, please try again later.");
-                                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT)
+                                Toast.makeText(getContext(), "No more users to display, please try again later.", Toast.LENGTH_SHORT)
                                         .show();
                                 NavHostFragment.findNavController(SecondFragment.this)
                                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
