46ad3cdf7b894fc2e91e38d848cfedfbaaacf6c6
Kushurando <mnietzlo@gmail.com>
2021-12-21 16:04:08 +0100

    update

--- MobileApp/app/src/main/java/com/walkwithme/SecondFragment.java
+++ MobileApp/app/src/main/java/com/walkwithme/SecondFragment.java
@@ -144,7 +144,7 @@ public class SecondFragment extends Fragment {
 
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                                VolleyLog.d(TAG, "No more users to display, please try again later.");
                                 Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT)
                                         .show();
                                 // hide the progress dialog
